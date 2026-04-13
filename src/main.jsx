d5b4c9ce1aa61e201845b8bcadd61af0b99cabc7
Alphatom-dynamics <alphatomdynamics@gmail.com>
2026-04-13 22:58:04 +0500

    Refactor import statement for ReactGA to use namespace import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ReactGA from "react-ga4";
-
+import * as ReactGA from "react-ga4";
 import "./index.css";
 import App from "./App.jsx";
 
